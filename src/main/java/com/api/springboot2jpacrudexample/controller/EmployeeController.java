37d297a0f496b67742a8455d6d00df64dce765f7
Jeremy <gencavjj@gmail.com>
2019-06-18 22:53:24 -0400

    added CORS wiring

--- src/main/java/com/api/springboot2jpacrudexample/controller/EmployeeController.java
+++ src/main/java/com/api/springboot2jpacrudexample/controller/EmployeeController.java
@@ -8,20 +8,14 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.api.springboot2jpacrudexample.exception.ResourceNotFoundException;
 import com.api.springboot2jpacrudexample.model.Employee;
 import com.api.springboot2jpacrudexample.repository.EmployeeRepository;
 
 @RestController
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RequestMapping("/api/v1")
 public class EmployeeController {
     @Autowired
